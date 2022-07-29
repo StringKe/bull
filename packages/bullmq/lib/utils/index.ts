493ce8102d09583e11d011002de977c21f6363f7
陈泉 <authemail@qq.com>
2022-07-29 14:21:51 +0800

    feat(bullmq): add custom worker

--- packages/bullmq/lib/utils/index.ts
+++ packages/bullmq/lib/utils/index.ts
@@ -1,3 +1,4 @@
 export * from './get-queue-options-token.util';
 export * from './get-queue-scheduler-token.util';
 export * from './get-shared-config-token.util';
+export * from './get-custom-worker-module.utils';
